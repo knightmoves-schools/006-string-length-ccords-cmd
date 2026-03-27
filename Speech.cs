0977d50fd8944518552dc9c0fad6150e7c2eae7c
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-27 14:16:32 -0500

    Update Speech.cs

--- Speech.cs
+++ Speech.cs
@@ -4,7 +4,7 @@ public class Speech
     public const string Quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
     public string PrintLengthAndTwentySecondLetter(){
-        //Add your code here
-        return Quote[21]
+        public string quotelength = Quote.length;
+        return Quote[21];
     }
 }
